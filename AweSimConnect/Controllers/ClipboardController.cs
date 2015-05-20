dc32c81a28d76cf85954eab5ae0f5c3715fd293b
Brian McMichael <brian@brianmcmichael.com>
2015-05-20 16:14:23 -0400

    Add Json.Net dependency for parsing clipboard data.

--- AweSimConnect/Controllers/ClipboardController.cs
+++ AweSimConnect/Controllers/ClipboardController.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace AweSimConnect.Controllers
 {
+    //TODO Reference JSON.NET in about. MIT license.  http://www.newtonsoft.com/json
+
+    /*
+     * This controller is used to manage clipboard actions and parsing. 
+     */
     class ClipboardController
     {
+
+
     }
 }

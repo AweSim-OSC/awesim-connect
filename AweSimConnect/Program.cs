c2ed3273da812b3f4a5ea7097b3bc966338abd91
brianmcmichael <brian@brianmcmichael.com>
2015-06-25 11:52:56 -0400

    Begin work on app single-instancing. Add appGuid and mutexId to program vars.

--- AweSimConnect/Program.cs
+++ AweSimConnect/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using AweSimConnect.Views;
 
@@ -12,6 +14,15 @@ namespace AweSimConnect
         [STAThread]
         static void Main()
         {
+            // get application GUID as defined in AssemblyInfo.cs
+            string appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+
+            // unique id for global mutex - Global prefix means it is global to the machine
+            string mutexId = string.Format("Global\\{{{0}}}", appGuid);
+
+            // Need a place to store a return value in Mutex() constructor call
+            bool createdNew;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new AweSimMain2());

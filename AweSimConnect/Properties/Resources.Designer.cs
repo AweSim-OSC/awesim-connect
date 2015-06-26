7de27c8aa8d6440fc28c6e3ff12e898342c64f0c
brianmcmichael <brian@brianmcmichael.com>
2015-06-26 11:32:15 -0400

    First draft of developer build instructions in README

--- AweSimConnect/Properties/Resources.Designer.cs
+++ AweSimConnect/Properties/Resources.Designer.cs
@@ -173,16 +173,6 @@ namespace AweSimConnect.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap header {
-            get {
-                object obj = ResourceManager.GetObject("header", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         public static System.Drawing.Bitmap info_gry {
             get {
                 object obj = ResourceManager.GetObject("info_gry", resourceCulture);

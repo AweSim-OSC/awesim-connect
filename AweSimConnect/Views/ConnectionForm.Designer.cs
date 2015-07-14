fd1b8b5f9669688c78e215a46477e3354837753f
Brian McMichael <brian@brianmcmichael.com>
2015-07-14 12:30:41 -0400

    Disallow connection form maximize.

--- AweSimConnect/Views/ConnectionForm.Designer.cs
+++ AweSimConnect/Views/ConnectionForm.Designer.cs
@@ -40,6 +40,7 @@
             this.ClientSize = new System.Drawing.Size(258, 14);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ConnectionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Connection";

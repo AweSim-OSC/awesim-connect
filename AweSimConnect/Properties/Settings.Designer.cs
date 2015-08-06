60026200629265f6b19a0ad311f063a08f866504
Brian McMichael <brian@brianmcmichael.com>
2015-08-06 16:46:19 -0400

    Enable 1-click settings by default.

--- AweSimConnect/Properties/Settings.Designer.cs
+++ AweSimConnect/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace AweSimConnect.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AutoOpenApp {
             get {
                 return ((bool)(this["AutoOpenApp"]));
@@ -120,7 +120,7 @@ namespace AweSimConnect.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool LaunchSSHOnImport {
             get {
                 return ((bool)(this["LaunchSSHOnImport"]));

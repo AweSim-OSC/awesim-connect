98a129187e3a94bbcc45b1794815c6570c86865e
Brian McMichael <brian@brianmcmichael.com>
2015-08-14 13:25:51 -0400

    Version check implemented. Needs error handling for no internet connectivity.

--- AweSimConnect/Controllers/VersionChecker.cs
+++ AweSimConnect/Controllers/VersionChecker.cs
@@ -7,11 +7,15 @@ namespace AweSimConnect.Controllers
 {
     class VersionChecker
     {
+        // The version response php is hosted here. A get response will return the format: 0.61.0.0
         public static string VERSION_RESPONSE_PAGE =
             @"https://apps.awesim.org/assets/wiag/connect/latest/awesimconnectversion.php";
 
+        // The current version of the running client.
+        private static string CLIENT_VERSION = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-        public static string GetVersion()
+
+        private static string GetRemoteVersion()
         {
             WebClient client = new WebClient();
             byte[] html = client.DownloadData(VERSION_RESPONSE_PAGE);
@@ -19,6 +23,31 @@ namespace AweSimConnect.Controllers
             return utf.GetString(html);
         }
 
+        private static bool isNewerVersion(string remoteVersion)
+        {
+            bool newVersion = false;
+
+            // Parsing format XX.XX.XX.XX
+            string[] remoteVersionArray = remoteVersion.Split('.');
+            string[] localVersionArray = CLIENT_VERSION.Split('.');
+
+            if (Int32.Parse(remoteVersionArray[0]) > Int32.Parse(localVersionArray[0]))
+            {
+                newVersion = true;
+            }
+
+            if (!newVersion && (Int32.Parse(remoteVersionArray[1]) > Int32.Parse(localVersionArray[1])))
+            {
+                newVersion = true;
+            }
+
+            return newVersion;
+        }
+
+        public static bool IsNewerVersionAvailable()
+        {
+            return isNewerVersion(GetRemoteVersion());
+        }
 
     }
 }
--- AweSimConnect/Views/AweSimMain2.cs
+++ AweSimConnect/Views/AweSimMain2.cs
@@ -123,6 +123,7 @@ namespace AweSimConnect.Views
             }
 
             timerMain.Start();
+            
         }
 
 

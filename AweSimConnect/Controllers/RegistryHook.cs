acf1b13d64fb7f5f2c1357a925666655b0b44bce
Brian L. McMichael <brian@brianmcmichael.com>
2016-04-08 10:35:06 -0400

    Refactorings.

--- AweSimConnect/Controllers/RegistryHook.cs
+++ AweSimConnect/Controllers/RegistryHook.cs
@@ -11,21 +11,27 @@ namespace AweSimConnect.Controllers
     class RegistryHook
     {
         // The URL handler for this app
-        private static string URI_PREFIX = "osc://";
         private static string CONNECT_REG_PROTOCOL = "osc";
-        private static string CONNECT_REG_POS = "Software\\Classes\\" + CONNECT_REG_PROTOCOL;
-
+        
         // The URL handler for this app
         // TODO: This is here for backwards compatibility
-        private static string URI_PREFIX_AWESIM = "awesim://";
         private static string CONNECT_REG_PROTOCOL_AWESIM = "awesim";
-        private static string CONNECT_REG_POS_AWESIM = "Software\\Classes\\" + CONNECT_REG_PROTOCOL_AWESIM;
-
+                
         private bool _installed;
 
         public RegistryHook()
+        {            
+            _installed = installHook(CONNECT_REG_PROTOCOL) && installHook(CONNECT_REG_PROTOCOL_AWESIM);
+        }
+                
+        private static string getURIPrefix(string protocol)
         {
-            _installed = installHook(CONNECT_REG_PROTOCOL, CONNECT_REG_POS) && installHook(CONNECT_REG_PROTOCOL_AWESIM, CONNECT_REG_POS_AWESIM);
+            return $"{protocol}://";
+        }
+
+        private static string getRegistryPosition(string protocol)
+        {
+            return $"Software\\Classes\\{protocol}";
         }
 
         /* Key Location:
@@ -46,15 +52,15 @@ namespace AweSimConnect.Controllers
          *                              Data: "[PATH]" "%1"
          */
 
-        private static bool installHook(string keyProtocol, string keyPosition)
+        private static bool installHook(string protocol)
         {
             try
             {
-                RegistryKey rKey = Registry.CurrentUser.CreateSubKey(keyPosition);
-                rKey.SetValue("", "URL: " + keyProtocol + " Protocol");
+                RegistryKey rKey = Registry.CurrentUser.CreateSubKey(getRegistryPosition(protocol));
+                rKey.SetValue("", $"URL: {protocol} Protocol");
                 rKey.SetValue("URL Protocol", "");
-                rKey.CreateSubKey("DefaultIcon").SetValue("", "\"" + Application.ExecutablePath + "\",0");
-                rKey.CreateSubKey(@"shell\open\command").SetValue("", "\"" + Application.ExecutablePath + "\" \"%1\"", RegistryValueKind.String);
+                rKey.CreateSubKey("DefaultIcon").SetValue("", $"\"{Application.ExecutablePath}\",0");
+                rKey.CreateSubKey(@"shell\open\command").SetValue("", $"\"{Application.ExecutablePath}\" \"%1\"", RegistryValueKind.String);
 
                 if (rKey != null)
                 {
@@ -73,16 +79,16 @@ namespace AweSimConnect.Controllers
         internal static string RemovePrefix(string prefixedString)
         {
             string newString = prefixedString;
-            newString = Regex.Replace(newString, RegistryHook.URI_PREFIX, "", RegexOptions.IgnoreCase);
+            newString = Regex.Replace(newString, getURIPrefix(CONNECT_REG_PROTOCOL), "", RegexOptions.IgnoreCase);
             // TODO the second replace is here because we are maintaining backwards compatibility
-            newString = Regex.Replace(newString, RegistryHook.URI_PREFIX_AWESIM, "", RegexOptions.IgnoreCase);
+            newString = Regex.Replace(newString, getURIPrefix(CONNECT_REG_PROTOCOL_AWESIM), "", RegexOptions.IgnoreCase);
             return newString;
         }
 
         // Return true if a string contains the URI prefix
         internal static bool ContainsPrefix(string candidate)
         {
-            return candidate.Contains(URI_PREFIX) || candidate.Contains(URI_PREFIX_AWESIM);
+            return candidate.Contains(getURIPrefix(CONNECT_REG_PROTOCOL)) || candidate.Contains(getURIPrefix(CONNECT_REG_PROTOCOL_AWESIM));
         }
 
         // true if the registry key has installed successfully

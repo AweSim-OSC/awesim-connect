fc1b8d38b8a60254784306dab8e56352f5dd1591
Brian L. McMichael <brian@brianmcmichael.com>
2016-04-08 15:16:57 -0400

    change icon on connection form

--- OSCConnect/Views/ConnectionForm.Designer.cs
+++ OSCConnect/Views/ConnectionForm.Designer.cs
@@ -33,13 +33,14 @@
             // 
             // ConnectionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(258, 14);
+            this.ClientSize = new System.Drawing.Size(387, 22);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "ConnectionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
